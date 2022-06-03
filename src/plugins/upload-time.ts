9c3789abe49ad865bb7d7c0885c65595a999bf06
Miguel Valera <correomiguelvalera@gmail.com>
2022-06-03 20:49:42 +0200

    the end

--- src/plugins/upload-time.ts
+++ src/plugins/upload-time.ts
@@ -7,43 +7,36 @@ import {
 } from 'fastify';
 
 
-export function buildUploadPlugin(dbNoSql: NOSQL_DB){
+export function buildUploadPlugin(dbNoSql: NOSQL_DB) {
 
-    return async function uploadPlugin(server:FastifyInstance, opts:FastifyRegisterOptions<Record<string,unknown>>, next: () => void) {
-     server.route({
+    return async function uploadPlugin(server: FastifyInstance, opts: FastifyRegisterOptions<Record<string, unknown>>, next: () => void) {
+        server.route({
             method: 'POST',
             url: '/upload',
             handler: async function (request: FastifyRequest, reply: FastifyReply) {
-                
-                const username=request.headers.username;
+
+                const username = request.headers.username;
                 const time = request.headers.time;
 
                 let json;
-                
-               if(username && time) json = {username: username, time: time}
-               
-               if(json){
 
+                if (username && time) json = { username: username, time: time }
 
-                const checkIfExists = await dbNoSql.getCollection().findOne({username: json.username});
+                if (json) {
+                    const checkIfExists = await dbNoSql.getCollection().findOne({ username: json.username });
 
-                if(checkIfExists){
-                    if(json.time<checkIfExists.time)await dbNoSql.getCollection().updateOne({username: json.username},{$set: {time: json.time}});
-                }else{
-                    await dbNoSql.getCollection().insertOne(json);
-                } 
+                    if (checkIfExists) {
+                        if (json.time < checkIfExists.time) await dbNoSql.getCollection().updateOne({ username: json.username }, { $set: { time: json.time } });
+                    } else {
+                        await dbNoSql.getCollection().insertOne(json);
+                    }
+                }
+                reply.header('Access-Control-Allow-Origin', '*');
+                reply
+                    .code(201)
+                    .headers({ 'content-type': 'application/json' })
+                    .send('OK');
 
-
-
-               } 
-   
-               
-                    reply.header('Access-Control-Allow-Origin', '*');
-                    reply
-                        .code(201)
-                        .headers({ 'content-type': 'application/json' })
-                        .send('OK');
-                
             }
         })
     };
